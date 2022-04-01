5ac08ef317080995e66366d0341d01e52fe5783d
Anton Lapin <anthonylapin11@gmail.com>
2022-04-01 09:53:13 +0200

    removed task description

--- interview/interview.ComputerScience/Program.cs
+++ interview/interview.ComputerScience/Program.cs
@@ -1,11 +1,3 @@
-﻿// Given an unsorted array of integers.
-// Find third largest number in array.
-// Try to use most optimized solution.
-// example: [4, 5, 1, 2, 9, 6]. Answer: 5, since 9 is largest and 6 is second largest.
-// Restrictions: You can use just plain C#, do not use LINQ.
-
-int[] arr = {
+﻿int[] arr = {
       9, 2, 5, 2, 1, 15, 8, 0, -1, -7, 3, 5, 10, 1
 };
-
-Console.WriteLine("Third max element is: ...");
