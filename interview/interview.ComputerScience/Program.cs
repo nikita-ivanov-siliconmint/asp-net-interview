dc312a0957197f679ffa03f79286925ffd899fbc
Anton Lapin <anthonylapin11@gmail.com>
2022-04-01 10:55:43 +0200

    added tests for different arrays

--- interview/interview.ComputerScience/Program.cs
+++ interview/interview.ComputerScience/Program.cs
@@ -1,3 +1,37 @@
-﻿int[] arr = {
-      9, 2, 5, 2, 1, 15, 8, 0, -1, -7, 3, 5, 10, 1
-};
+﻿using System;
+using System.Diagnostics;
+
+namespace interview.ComputerScience
+{
+      internal class Program
+      {
+            static int Solution(int[] arr)
+            {
+                  return 0;
+            }
+
+            static void TestSolution()
+            {
+                  (int[], int)[] tests =
+                  {
+                        (new[] {9, 2, 5, 2, 1, 15}, 5),
+                        (new[] {-1, -6, -3, -5, -99, -4}, -4),
+                        (new[] {3, 2, 1, 5, 4}, 3),
+                        (new[] {7, 2, 1, 1, 2, 3, 5, 6, 9, -1, 99, 5}, 7)
+                  };
+
+                  foreach ((int[] arr, int expected) in tests)
+                  {
+                        int result = Solution(arr);
+                        string arrStr = $"[{string.Join(",", arr)}]";
+                        Debug.Assert(result == expected, $"Test failed for array {arrStr}.");
+                        Console.WriteLine($"Test for array {arrStr} has passed. Expected value: {expected}, found result: {result}");
+                  }
+            }
+            
+            static void Main(string[] args)
+            {
+                  TestSolution();
+            }
+      }
+}
